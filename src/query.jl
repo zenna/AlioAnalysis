4e4a4259e697da1425120db10073ef5aa8dca045
Zenna Tavares <zenna@mit.edu>
2017-11-07 17:53:38 -0500

    added compare pi

--- src/query.jl
+++ src/query.jl
@@ -3,57 +3,80 @@ using DataFrames
 using DataArrays
 using TSne
 using Plots
-
-df = DataFrame(name=["John", "Sally", "Kirk"], age=[23., 42., 59.], children=[3,5,2])
-losses = DataFrame(run=["run1", "run2", "run3"], )
-
-function join(df)
-  x = @from i in df begin
-      @where i.age > 0
-      @select {i.name, i.children}
-      @collect DataFrame
-  end
-end
-
-# 1. Design data layout
-# 2. Fix Tsne
-# 3. # negro please
-
-function test_analysis(carr=xy_plus_x_arr())
-  # 1 invert it
-  inv = aprx_invert(carr)
-end
-
+using Arrows
 using NLopt
 
-function appenddata()
+# function join(df)
+#   x = @from i in df begin
+#       @where i.age > 0
+#       @select {i.name, i.children}
+#       @collect DataFrame
+#   end
+# end
+
+"Standard callback that records"
+function stdrecordcb()
   df = DataFrame(iteration = Int[],
                  loss = Float64[],
                  systime = Float64[],
                  input = Vector{Float64}[],
                  output = Vector{Float64}[])
   i = 0
-  function update_data(data)
+  function stdupdate(data)
+   println("AGOT HEREB")
     push!(df, [i, data.loss, time(), [data.input...], [data.output...]])
     i = i + 1
   end
-  df, update_data
+  df, stdupdate
 end
 
-function test_tsne(carr = TestArrows.xy_plus_x_arr())
+function min_naive(carr = TestArrows.xy_plus_x_arr())
   @show naive_loss, ϵid = Arrows.naive_loss(carr, 1.0)
   ϵprt = ◂(naive_loss, ϵid)
-  df, update_data = appenddata()
+  df, update_data = stdrecordcb()
   Arrows.optimize(naive_loss, ▸(naive_loss), ϵprt, rand(2); callbacks = [update_data])
-  df
+  [df]
 end
 
-function tsne_plot(carr::Arrow, dist::Symbol, nruns::Integer)
-  results = [test_tsne(carr) for i = 1:nruns]
-  concatted = vcat([df[dist] for df in results]...)
-  points = tsne(Array(concatted), (x, y)->(sum(abs.(x-y))))
-  @show size(points)
-  @show size.(results)
+"Minimize `arr` using to"
+function min_domainϵ(arr = TestArrows.xy_plus_x_arr(), inputs=rand(length(▸(arr, !is(θp)))))
+  outs = arr(inputs...)
+  @show dmloss, ϵid = domain_ovrl(arr)
+  invarr = invert(arr)
+  aprx_totalize!(invarr)
+  idloss = id_loss(arr, invarr)
+
+  # Callback generator which records domain and id loss
+  function domaincallback()
+    df2 = DataFrame(iteration = Int[],
+                   idloss = Float64[],
+                   domainloss = Float64[])
+    i = 0
+    function domainupdate(data)
+      println("GOT HEREB")
+      # compute the domain loss and id loss
+      dm_error = dmloss(data.input...)[ϵid]
+      id_error = idloss(data.input...)[1]
+      push!(df2, [i, dm_error, id_error])
+      i = i + 1
+    end
+    df2, domainupdate
+  end
+
+  
+  df, stdupdate = stdrecordcb()
+  # @show update_data(3)
+  domaindf, domupdate = domaincallback()
+  # @show update_data(3)
+
+  init = [outs..., rand(length(▸(dmloss, is(θp))))...]
+  optimize(dmloss, ▸(dmloss, is(θp)), ◂(dmloss, ϵid), init;
+            callbacks=[stdupdate, domupdate])
+  [df, domaindf]
+end
+
+"Scatter many points"
+function scattermany(points)
   lb = 1
   local plot
   for res in results
@@ -65,4 +88,28 @@ function tsne_plot(carr::Arrow, dist::Symbol, nruns::Integer)
     lb = ub + 1
   end
   plot
+end
+
+# function ok(carr::Arrow, dist::Symbol, nruns::Integer, gendata::Function)
+#   results = [gendata(carr) for i = 1:nruns]
+#   concatted = vcat([df[dist] for df in results]...)
+#   points = tsne(Array(concatted), (x, y)->(sum(abs.(x-y))))
+#   @show size(points)
+#   @show size.(results)
+# end
+
+function compare_pi_naive(carr::Arrow, nruns::Integer)
+  naivedata = min_naive(carr)
+  domaindata = min_domainϵ(carr)
+  naivedata, domaindata
+end
+
+function manyjoin(on::Symbol, dfs::DataFrame...)
+  x = first(dfs)
+  for df in dfs[2:end]
+    @show typeof(x)
+    @show typeof(df)
+    x = join(x, df, on)
+  end
+  x
 end

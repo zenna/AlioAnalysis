2a8695e42a16633be4031f873bb9ede615d60b24
Zenna Tavares <zennatavares@gmail.com>
2018-01-27 23:55:56 -0500

    pgf training

--- src/transform/pgf.jl
+++ src/transform/pgf.jl
@@ -1,22 +1,40 @@
+function okok(tabv::Arrows.TraceAbValues, arr::Arrow)::NmAbValues
+  # Assume theres only one tabv that corresponds to 
+  tsprts_set = map(Arrows.trace_sub_ports, keys(tabv))
+  ids = Int[]
+  for sprt in ⬨(arr)
+    idx = findfirst(tsprts_set) do tsprts
+      sprt ∈ map(Arrows.sub_port, tsprts)
+    end
+    @assert idx != 0
+    push!(ids, idx)
+  end
+  abv = collect(values(tabv))
+  @show ids
+  Arrows.NmAbValues(port_sym_name(prt) => abv[ids[prt.port_id]] for prt in ⬧(arr))
+end
+
 "Computes `δ(pgf(x), n(f(x))`"
 function δpgfx_ny_arr(f::Arrow, xabv::XAbValues)
   invf = invert(f, inv, xabv)
-  xabv = traceprop!(invf, xabv)
+  tabv = traceprop!(invf, xabv)
+  nabv = okok(tabv, invf)
   n = pslnet(invf)
   lossarr = δny_x_arr(n; nm = :δpgfx_ny_arr)
-  lossarr, n, xabv
+  lossarr, n, nabv
 end
 
 "Iterator of `[f(x), pgf(x)]`  values from `xgenss`: Iterator over inputs "
-function x_to_y_θ_gen(pgfarr::Arrow, xgens)
+function x_to_y_θ_gen(pgfarr::Arrow, xgen)
   θprts, yprts = Arrows.partition(is(θp), ◂(pgfarr))
-  θoids = out_port_id.(θprts)
-  yoids = out_port_id.(yprts)
-  pgfjl = il(julia(pgfarr))
-  function splitoutvals(xs::Vector)
-    [out[i] for i in yoids], [out[i] for i in θoids]
+  @grab pgfarr
+  @grab θoids = out_port_id.(θprts)
+  @grab yoids = out_port_id.(yprts)
+  @grab pgfjl = il(Arrows.splat(julia(pgfarr)))
+  function splitoutvals(xs::Tuple)
+    [[xs[i] for i in yoids]; [xs[i] for i in θoids]]
   end
-  imap(splitoutvals ∘ pgfjl, xgens)
+  imap(splitoutvals ∘ pgfjl, xgen)
 end
 
 """
@@ -36,8 +54,8 @@ function trainpgfnet(lossarr::Arrow,
                      optimizeargs...)
   # @pre same([n◂(lossarr), n▸(n)]) # What should these be?
   nnettarr = first(Arrows.findtarrs(lossarr, n))
-  # @grab xabv
   @grab nnettarr
+  @grab xabv
   @grab tabv = Arrows.tabvfromxabv(nnettarr, xabv)
 
   optimizenet(lossarr,
@@ -58,13 +76,21 @@ function test_pgf_training(f = TestArrows.xy_plus_x_arr())
   sz = [batch_size, 1]
   xabv = NmAbValues(pnm => AbValues(:size => Size(sz)) for pnm in port_names(f))
   pgff = pgf(f, Arrows.pgf, xabv)
-  lossarr, n, tabv = δpgfx_ny_arr(f, xabv)
-  xgens = [Sampler(()->rand(sz...)) for i = 1:n▸(f)]
-  y_θ_gen = x_to_y_θ_gen(pgff, xgens)
+  lossarr, n, xabv = δpgfx_ny_arr(f, xabv)
+  @grab lossarr
+  @grab n
+  @grab xabv
+  
+  # @grab tabv
+  # @assert false
+  xgen = Sampler(()->[rand(sz...) for i = 1:n▸(f)])
+  y_θ_gen = x_to_y_θ_gen(pgff, xgen)
+  @grab xgen
+  @grab y_θ_gen
   trainpgfnet(lossarr,
               n,
               y_θ_gen,
-              tabv,
+              xabv,
               TensorFlowTarget.TFTarget,
               TensorFlowTarget.mlp_template)
 end

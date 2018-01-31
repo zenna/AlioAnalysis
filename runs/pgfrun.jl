5f662023890c43a8ea34bea7ad4ab3311151988b
Zenna Tavares <zennatavares@gmail.com>
2018-01-31 01:51:10 -0500

    runcommon and pgfruns

--- runs/pgfrun.jl
+++ runs/pgfrun.jl
@@ -1,64 +1,47 @@
 using Arrows
+using AlioZoo
+const AZ = AlioZoo
 import TensorFlowTarget: mlp_template, TFTarget
 using AlioAnalysis
-using Spec
 const AA = AlioAnalysis
+using Spec
+
+include("runcommon.jl")
+
+# What's still wrong
+# Template
+# call to dispatch runs should take cmdline args
+# Continue when no improvement
 
 "Preimage attack using reparamterized parametric inverse"
-function pgftrainrpi(arr, xabv, xgen, opt; optimargs...)
-  lossarr, n, xabv = δpgfx_ny_arr(f, xabv, AA.meancrossentropy)
-  y_θ_gen = x_to_y_θ_gen(pgff, xgen)
-  trainpgfnet(lossarr,
-              n,
-              y_θ_gen,
-              xabv,
-              TensorFlowTarget.TFTarget,
-              TensorFlowTarget.conv_template;
-              optimargs...)
-end
-
-function initrun(opt::Dict{Symbol, Any})
-  # Generic data frames / callbacks
-  df, record = recordrungen(opt[:runname])
-  cbs = [record,
-         everyn(savedfgen(opt, df), 3),
-         everyn(printloss, 5)]
-
-  # Get the data from the bundle
-  fwdarr, xabv, xgen = opt[:bundle].fwdarr, opt[:bundle].xabv, opt[:bundle].xgen
-
-  # Setup opt with meta data
-  opt[:arrname] = name(opt[:fwdarr])
-  opt[:model] = opt[:trainfunc][1]
-  lstring = linearstring(opt, :runname,
-                              :niters,
-                              :model,
-                              :batch_size,
-                              :arrname,
-                              :traindatasize)
-  opt[:trainfunc][2](opt[:fwdarr], opt, xgen;
-                     callbacks = cbs,
-                     logdir = joinpath(opt[:logdir], lstring),
-                     cont = data -> data.i < opt[:niters])
+function pgftrainrpi(bundle; optimargs...)
+  @grab bundle
+  lossarr, n, xabv = AA.δpgfx_ny_arr(bundle.fwdarr, bundle.xabv, AA.meancrossentropy)
+  y_θ_gen = AA.x_to_y_θ_gen(bundle.pgff, bundle.gen)
+  AA.trainpgfnet(lossarr,
+                 n,
+                 y_θ_gen,
+                 xabv,
+                 TensorFlowTarget.TFTarget,
+                 TensorFlowTarget.conv_template;
+                 optimargs...)
 end
 
 "Generate data for initialization comparison"
 function genopts()
-  # Vary over different arrows, varying the initial conditions
-  optspace = Options(:bundle => AlioZoo.allbundles(),
+  optspace = Options(:bundlegen => AZ.allbundlegens,
                      :trainfunc => [(:rpi, pgftrainrpi)],
                      :traindatasize => [1, 2, 5, 40, 500],
                      :batch_size => [1, 32],
                      :niters => 1000)
 
   println(@__FILE__)
-  # Makekwrd non standard
   dispatchruns(optspace,
                @__FILE__,
-               initrun;
-               toenum=[:fwdarr, :trainfunc, :traindatasize, :batch_size],
-               runsbatch=true,
-               runnow=false,
+               commoninitrun;
+               toenum=[:bundlegen, :trainfunc, :traindatasize, :batch_size],
+               runsbatch=false,
+               runnow=true,
                runlocal=false,
                nsamples=1,
                group="iurat",
@@ -66,10 +49,10 @@ function genopts()
 end
 
 function main()
-  genorrun(genopts, initrun)
+  genorrun(genopts, commoninitrun)
 end
 
-# genopts()
+genopts()
 
 # How to do generalization test?
-main()
+# main()
---
+++ runs/runcommon.jl
@@ -0,0 +1,25 @@
+"Run initializer used by many runs"
+function commoninitrun(opt::Dict{Symbol, Any})
+  # Generic data frames / callbacks
+  df, record = recordrungen(opt[:runname])
+  cbs = [record,
+         everyn(savedfgen(opt, df), 3),
+         everyn(printloss, 5)]
+
+  # Get the data from the bundle
+  @grab bundle = opt[:bundlegen](; opt...)
+
+  # Setup opt with meta data
+  opt[:arrname] = name(bundle.fwdarr)
+  opt[:model] = opt[:trainfunc][1]
+  lstring = linearstring(opt, :runname,
+                              :niters,
+                              :model,
+                              :batch_size,
+                              :arrname,
+                              :traindatasize)
+  opt[:trainfunc][2](bundle;
+                     callbacks = cbs,
+                     logdir = joinpath(opt[:logdir], lstring),
+                     cont = data -> data.i < opt[:niters])
+end
